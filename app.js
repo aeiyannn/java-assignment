0541e8d297aff206606dfdf35ca40a796f6443a5
Aeiyan Khan <110762239+aeiyannn@users.noreply.github.com>
2022-10-19 00:23:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// Task 1
+// 1
+var username
+//2
+var myname="Aeiyan khan";
+//3
+var message="Hello world";
+alert(message)
+//4
+var name='Aeiyan';
+var year='17 yearold';
+var course='web and app';
+alert(name)
+alert(year)
+alert(course)
+//5
+alert('pizza \npizz \npiz \npi \np')
+//6
+var  email='My email address is aeiyankhan2@gmail.com';
+alert(email)
+//7
+var book='Iam trying to learn from book A smarter way to learn JavaScript';
+alert(book)
+//8
+document.write('yah! I can write HTML content through javascript')
+//9
+alert('“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”')
+//Task 2
+//1
+alert('Hi welcome')
+//2
+alert('error! please enter a valid password')
+//3
+alert('Welcome to js land... \nHappy coding!')
+//4
+alert('welcome to jsland...')
+alert('Happy coding')
+//5
+console.log('Hello... I can run js through my web  browser,s console')
+
+
